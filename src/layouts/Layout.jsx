3be01c7372a93c950f4db653422ff40407287e5c
livemehere <rhdxoals@gmail.com>
2022-08-03 23:25:22 +0900

    CD : deploy

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,10 +1,6 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { Link, useLocation, useParams } from 'react-router-dom';
-import { AiOutlineHome, AiFillHome } from 'react-icons/ai';
-import { useNavigate } from 'react-router-dom';
-import { ImSpinner9 } from 'react-icons/im';
-import Spinner from '../components/Spinner';
 
 const Layout = ({ children }) => {
   const [headerTrigger, setHeaderTrigger] = useState(false);
